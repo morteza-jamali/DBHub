3c2787bd1465a0632653bee7d2c44e4057df518b
morteza-jamali <mortezajamali4241@gmail.com>
2020-12-22 10:59:57 +0330

    vercel directory structure fixed

---
+++ vercel/README.md
@@ -0,0 +1,1 @@
+### DBHub RESTful API
--- vercel/api/index.ts
+++ vercel/api/index.ts
@@ -1,6 +1,6 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
-export default (request: NowRequest, response: NowResponse) => {
+export default (_request: NowRequest, response: NowResponse) => {
   response.setHeader('Content-Type', 'application/json');
-  //const { name = 'World' } = request.query;
+  response.status(200).send({name: 'Morteza Jamali'});
 };
