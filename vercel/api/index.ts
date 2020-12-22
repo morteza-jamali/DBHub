9015f5c55a6c97f546ba22b12ecc7274fd25ef3e
morteza-jamali <mortezajamali4241@gmail.com>
2020-12-22 11:47:13 +0330

    loopBack framework inited

--- vercel/api/index.ts
+++
@@ -1,6 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-
-export default (_request: NowRequest, response: NowResponse) => {
-  response.setHeader('Content-Type', 'application/json');
-  response.status(200).send({name: 'Morteza Jamali'});
-};
